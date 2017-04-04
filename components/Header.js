1667dba4d8f58e58a977cf18cb0037e26d74145b
vanita5 <mail@vanit.as>
2017-04-04 15:09:43 +0200

    remove Header

--- components/Header.js
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-    <div>
-        <h1>Twitter - Mastodon</h1>
-    </div>
-)
-
-export default Header
--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,14 @@
 import Head from 'next/head'
 
-import Header from './Header'
-
-const Layout = props => (
+const Layout = ({ children }) => (
     <div>
         <Head>
             <title>Twitter - Mastodon Bridge</title>
             <meta charSet="utf-8"/>
             <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
-        <Header/>
-        {props.children}
+        
+        { children }
     </div>
 )
 
