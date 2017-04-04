3101ca2dae96c2b5a824831029c92ebe63b41782
vanita5 <mail@vanit.as>
2017-04-04 15:19:48 +0200

    add nprogress

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Router from 'next/router'
 
-const Layout = ({ children }) => (
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = url => {
+    console.log(`Loading ${url}`)
+    NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
+
+export default ({ children }) => (
     <div>
         <Head>
             <title>Twitter - Mastodon Bridge</title>
             <meta charSet="utf-8"/>
             <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+
+            <link rel="stylesheet" type="text/css" href="/static/nprogress.css"/>
         </Head>
-        
+
+        <h1>Twitter - Mastodon</h1>
         { children }
     </div>
 )
-
-export default Layout
