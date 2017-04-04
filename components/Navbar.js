ce438f06e61053f535bea39596a688b892a611d7
vanita5 <mail@vanit.as>
2017-04-04 22:11:12 +0200

    finish navbar for now

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,6 +8,18 @@ export default () => (
             <div className="logo">
                 <Link href="/"><Icon glyph="plug"/></Link>
             </div>
+            <div className="menu">
+                <ul>
+                    <li className="root">Menu</li>
+                    <li className="sub">
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className="sub">
+                        <Link href="/about">About</Link>
+                    </li>
+                </ul>
+                <hr/>
+            </div>
         </nav>
     </div>
 )
