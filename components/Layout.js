ba4452d2ccb8fcf2e22d538babf822b5922f4508
vanita5 <mail@vanit.as>
2017-04-04 15:06:37 +0200

    add head

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+
 import Header from './Header'
 
-const layoutStyle = {}
-
 const Layout = props => (
-    <div style={layoutStyle}>
+    <div>
+        <Head>
+            <title>Twitter - Mastodon Bridge</title>
+            <meta charSet="utf-8"/>
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
         <Header/>
         {props.children}
     </div>
