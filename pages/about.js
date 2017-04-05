7d0b67883d6e76df47d0e5a47b9aef499b1dc24b
vanita5 <mail@vanit.as>
2017-04-05 15:57:49 +0200

    remove the navbar

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
 import Layout from '../components/Layout'
-import Navbar from '../components/Navbar'
 
 export default props => (
     <Layout>
-        <Navbar url={props.url.pathname}/>
     </Layout>
 )
