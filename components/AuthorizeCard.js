b0d3650d8b15dc8dd090269b4ffdcbc081cc7e58
remmy <rem@remics.net>
2017-04-09 17:41:27 +0200

    prettier cards (logos) with unified component

---
+++ components/AuthorizeCard.js
@@ -0,0 +1,46 @@
+import {
+    Card,
+    CardText,
+    CardBlock,
+    CardTitle,
+    CardSubtitle,
+    Button
+} from 'reactstrap';
+
+type Props = {
+    serviceName: string,
+    authorized: bool,
+    authorize: Function,
+    backColor: string,
+    img: string,
+};
+
+const AuthorizeCard = ({ serviceName, authorized, authorize, backColor, img }) => (
+    <Card style={style.card}>
+        <div style={style.image(img, backColor)} className="card-img-top"/>
+        <CardBlock>
+            <CardTitle>{serviceName}</CardTitle>
+            <CardSubtitle>{authorized ? `Authorized!` : `Not yet connected.`}</CardSubtitle>
+            <CardText>{`Please authorize with ${serviceName}.`}</CardText>
+            <Button onClick={authorize}>Authorize</Button>
+        </CardBlock>
+    </Card>
+);
+export default AuthorizeCard;
+
+const style = {
+    card: {
+        width: 400,
+        margin: '0 auto',
+    },
+    image: (src, backgroundColor) => ({
+        display: 'inline-block',
+        width: '100%',
+        height: 266,
+        backgroundImage: `url(/static/img/${src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+        backgroundColor,
+    }),
+};
--- components/Description.js
+++ components/Description.js
@@ -1,43 +1,38 @@
-import React from 'react'
+import  { PureComponent } from 'react';
 
-import SubHeader from './SubHeader'
-import TwitterCard from './TwitterCard'
-import MastodonCard from './MastodonCard'
+import SubHeader from './SubHeader';
+import AuthorizeCard from './AuthorizeCard';
 
-import DatabaseWrapper from '../lib/DatabaseWrapper'
+import DatabaseWrapper from '../lib/DatabaseWrapper';
 
-import { Row, Col } from 'reactstrap'
+import { Row, Col } from 'reactstrap';
 
-export default class Description extends React.Component {
-    constructor(props) {
-        super(props)
-        this.db = new DatabaseWrapper()
-        this.state = {
-            twitterAuthorized: false,
-            mastodonAuthorized: false
-        }
-    }
+export default class Description extends PureComponent {
+    db = new DatabaseWrapper();
+    state = {
+        twitterAuthorized: false,
+        mastodonAuthorized: false,
+    };
 
-    componentDidMount() {
-        this.getAuthorization()
+    componentWillMount() {
+        this.getAuthorization();
     }
 
     getAuthorization() {
-        const self = this
         this.db.getAuthorization()
             .catch(err => console.log(err))
             .then(docs => {
                 console.log(docs)
                 docs.forEach(doc => {
                     if (!doc.service) return
-                    if (doc.service === 'twitter') self.setState({
+                    if (doc.service === 'twitter') this.setState({
                         twitterAuthorized: doc.access_token && doc.access_token.length > 0
-                    })
-                    else if (doc.service === 'mastodon') self.setState({
+                    });
+                    else if (doc.service === 'mastodon') this.setState({
                         mastodonAuthorized: doc.access_token && doc.access_token.length > 0
-                    })
-                })
-            })
+                    });
+                });
+            });
     }
 
     render() {
@@ -46,10 +41,20 @@ export default class Description extends React.Component {
                 <SubHeader/>
                 <Row style={{ marginTop: 75 }}>
                     <Col lg="6" xs="12">
-                        <TwitterCard authorized={this.state.twitterAuthorized}/>
+                        <AuthorizeCard
+                            serviceName="Twitter"
+                            authorize={() => {}} // TODO
+                            backColor="#1da1f2"
+                            img="twitter_card_logo.svg"
+                            authorized={this.state.twitterAuthorized}/>
                     </Col>
                     <Col lg="6" xs="12">
-                        <MastodonCard authorized={this.state.mastodonAuthorized}/>
+                        <AuthorizeCard
+                            serviceName="Mastodon"
+                            authorize={() => {}} // TODO
+                            backColor="#282c37"
+                            img="mastodon_card_logo.png"
+                            authorized={this.state.mastodonAuthorized}/>
                     </Col>
                 </Row>
             </div>
