5397b0fe4a7216da05efdb06a28494bb53a2d859
remmy <rem@remics.net>
2017-04-10 15:49:29 +0200

    implemented rudimentary twitter login

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -1,12 +1,54 @@
 // @flow
-import { getLink as getTwitterLink } from './twitterAuth';
+/* eslint camelcase: 0 */
+import { Map } from 'immutable';
+import {
+    accessToken as twitterAccessToken,
+    requestToken as twitterRequestToken,
+} from './twitterAuth';
 import express from 'express';
+import Twit from 'twit';
+
 const auth = express.Router();
+
+let authPending: Map<string, string> = Map();
 
 // twitter auth
 auth.route('/twitter').get(async (req, res) => {
-    const authLink = await getTwitterLink();
-    res.redirect(302, authLink);
+    const consumerKey = process.env.TWITTER_CONSUMER_KEY;
+    const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+    if (!consumerKey || !consumerSecret) {
+        res.redirect(302, 'http://localhost:3000/start');
+        return;
+    }
+
+    const { oauth_token, oauth_verifier } = req.query;
+    if (
+        req.query &&
+        oauth_token &&
+        oauth_verifier &&
+        authPending.has(oauth_token)
+    ) {
+        const reqSecret = authPending.get(oauth_token);
+        const twitAuth = await twitterAccessToken(
+            consumerKey,
+            consumerSecret,
+            oauth_token,
+            reqSecret,
+            oauth_verifier,
+        );
+        console.log(twitAuth);
+        const T = new Twit(twitAuth);
+        res.redirect(302, 'http://localhost:3000/start');
+        //TODO do stuff
+    } else {
+        const { token, secret, url } = await twitterRequestToken(
+            consumerKey,
+            consumerSecret,
+            'http://localhost:3000/auth/twitter',
+        );
+        authPending = authPending.set(token, secret);
+        res.redirect(302, url);
+    }
 });
 
 // mastodon auth
--- server/auth/twitterAuth.js
+++ server/auth/twitterAuth.js
@@ -1,7 +1,87 @@
 // @flow
+/* eslint camelcase: 0 */
+import { OAuth } from 'oauth';
 
-export const getLink = () =>
-    Promise.resolve(
-        'https://api.twitter.com/oauth/authenticate?oauth_token=NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0',
-        //TODO
+type RequestTokenResult = {
+    url: string,
+    token: string,
+    secret: string,
+};
+export const requestToken = async (
+    consumerKey: string,
+    consumerSecret: string,
+    callbackUrl: string,
+): Promise<RequestTokenResult> => {
+    const oauth = new OAuth(
+        'https://twitter.com/oauth/request_token',
+        'https://twitter.com/oauth/access_token',
+        consumerKey,
+        consumerSecret,
+        '1.0A',
+        callbackUrl,
+        'HMAC-SHA1',
     );
+
+    const {
+        token,
+        secret,
+    } = await new Promise((resolve, reject) => {
+        oauth.getOAuthRequestToken((error, token, secret, results) => {
+            if (error || !results.oauth_callback_confirmed === 'true') {
+                reject();
+            }
+            resolve({ token, secret });
+        });
+    });
+    return {
+        url: `https://twitter.com/oauth/authorize?oauth_token=${token}`,
+        token,
+        secret,
+    };
+};
+
+type AccessTokenResult = {
+    consumer_key: string,
+    consumer_secret: string,
+    access_token: string,
+    access_token_secret: string,
+};
+export const accessToken = async (
+    consumerKey: string,
+    consumerSecret: string,
+    reqToken: string,
+    reqSecret: string,
+    verifier: string,
+): Promise<AccessTokenResult> => {
+    const oauth = new OAuth(
+        'https://twitter.com/oauth/request_token',
+        'https://twitter.com/oauth/access_token',
+        consumerKey,
+        consumerSecret,
+        '1.0A',
+        null,
+        'HMAC-SHA1',
+    );
+    const {
+        token,
+        secret,
+    } = await new Promise((resolve, reject) => {
+        oauth.getOAuthAccessToken(
+            reqToken,
+            reqSecret,
+            verifier,
+            (error, token, secret) => {
+                if (error) {
+                    reject();
+                }
+                resolve({ token, secret });
+            },
+        );
+    });
+    return {
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token: token,
+        access_token_secret: secret,
+    };
+};
